2d6e1ec36a24e6c30232ae491fa7c92e532e558f
Tsunamins <reillyamr@gmail.com>
2019-08-13 14:22:40 -0400

    add validations to user

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
     has_secure_password
+
+    validates :username, presence: true
+    validates :username, uniqueness: true
+    validates :email, presence: true
+    validates :email, uniqueness: true
+    validates :favorite_wine, length: { maximum: 100 }
 end 
