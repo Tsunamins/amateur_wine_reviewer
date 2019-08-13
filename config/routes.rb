68f792779767a3a1405b1a438fa37b55f92dae31
Tsunamins <reillyamr@gmail.com>
2019-08-13 12:38:00 -0400

    fix signup path/routing etc

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  #for now 
-  get 'signup' => 'users#new'
-  post 'signup' => 'users#create'
+  
+  
+  get '/signup' => 'users#new' #current setup needed slash in front, no quotes around signup_path in home/index - signup link
+  post '/signup' => 'users#create'
   
   root 'home#index'
   resources :users
