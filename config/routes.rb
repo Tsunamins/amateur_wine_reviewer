8decbc09595102edcc7149ac28508aa3c53e385a
Tsunamins <reillyamr@gmail.com>
2019-08-13 11:51:25 -0400

    add home controller and view, add signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #for now 
+  get 'signup' => 'users#new'
+  post 'signup' => 'users#create'
+  
+  root 'home#index'
   resources :users
 end
