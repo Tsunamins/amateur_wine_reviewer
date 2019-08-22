1c4fe4eb866f5883a7758f276048cf25d68eb6da
Tsunamins <reillyamr@gmail.com>
2019-08-22 14:50:48 -0400

    add omniauth to user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,12 @@ class User < ActiveRecord::Base
     validates :email, presence: true
     validates :email, uniqueness: true
     validates :favorite_wine, length: { maximum: 100 }
+
+    def self.from_omniauth(auth)
+        where(email: auth.info.email).first_or_initialize do |user|
+          user.user_name = auth.info.name
+          user.email = auth.info.email
+          user.password = SecureRandom.hex
+        end
+      end
 end 
