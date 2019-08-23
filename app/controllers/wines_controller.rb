45245d55d011eee132653ba069d5f6a38cab829f
Tsunamins <reillyamr@gmail.com>
2019-08-23 13:24:51 -0400

    add necessary definitions to wine controller

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,17 +1,51 @@
 class WinesController < ApplicationController
+    def show
+        @wine = Wine.find(params[:id])
+    end
+
+    def index 
+        @wines = Wine.all
+    end 
 
     def new 
+        @user = current_user
+        @wine = Wine.new
     end 
 
     def create
+        @user = current_user
+        @wine = Wine.new(wine_params)
+        if @wine.save 
+            redirect_to @wine
+        else
+            render :new 
+        end
     end 
 
-    def index 
-    end 
-
+    
     def edit
+        @user = current_user
+        @wine = Wine.find(params[:id])
+
     end 
 
     def update 
+        @user = current_user
+        @wine = Wine.find(params[:id])
+
+        @wine.update(wine_params)
+
+        if @wine.save
+            redirect_to @wine
+        else
+            render :edit
+        end
+
     end 
+
+    private 
+
+    def wine_params
+        params.requre(:wine).permit([:wine_name, :vintage, :vineyard, :winemaker, :story, :grape, :region, :style, :alcohol])
+    end
 end 
