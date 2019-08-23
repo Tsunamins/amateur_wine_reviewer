36361f4316834c5d5cbdec925b4609a456a22bef
Tsunamins <reillyamr@gmail.com>
2019-08-23 13:29:44 -0400

    add validations to wine

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -2,4 +2,8 @@ class Wine < ActiveRecord::Base
     has_many :reviews
     has_many :users, through: :reviews
 
+    validates :vintage, length: { is: 4 }
+    validates :wine_name, length: { maximum: 100 }
+    validates :alcohol, length: { maximum: 5 } 
+    validates :story, length: { maximum: 1000 } 
 end 
