c732346cdb243484e37f8221bf66ea7c261739bd
Tsunamins <reillyamr@gmail.com>
2019-09-02 13:19:33 -0400

    unimportant changes

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,6 +2,9 @@ class Review < ActiveRecord::Base
     belongs_to :user
     belongs_to :wine
 
+    #redo validations here
+    #also adapt this: validates :stars, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than: 6}
+
 
     
 end
