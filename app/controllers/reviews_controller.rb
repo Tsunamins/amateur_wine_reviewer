76c0d01a1fa40bd968f7892646ed059826ee4fc5
Tsunamins <reillyamr@gmail.com>
2019-09-06 15:55:47 -0400

    delete uneccesary files

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,46 +1,30 @@
 
 class ReviewsController < ApplicationController
     def index 
-        
         @user = User.find_by(id: params[:id])
         @reviews = Review.all 
         
         @review = Review.find_by_id(params[:id])
     end 
 
-    
-
     def show 
-       
         @review = Review.find_by_id(params[:id])
-        
-        
     end 
 
     def new         
-        
         if params[:wine_id] && @wine = Wine.find_by_id(params[:wine_id])
             @review = @wine.reviews.build
           else
             @review= Review.new
             @review.build_wine
           end
-        
-       
-        
-        
-        
     end 
 
     def create       
-       
         @wine = Wine.find_by_id(params[:wine_id])
-        
         @review = current_user.reviews.build(review_params)
-        
 
         if @review.save 
-            
             redirect_to review_path(@review)
         else
             render :new 
@@ -50,25 +34,35 @@ class ReviewsController < ApplicationController
     def edit
         @user = current_user
         @review = Review.find(params[:id])
+    end 
        
+    def like 
+        @review = Review.find(params[:id])
+        type = params[:type]
+        if type == "like"
+            
+            current_user.likes << @review
+            
+
+        elsif type == "unlike"
+            current_user.likes.delete(@review)
+             
+
+        else
+            # Type missing, nothing happens
+             
+        end
+    end
        
 
-    end 
 
     def update 
         @review = Review.find(params[:id])
-
         @wine = Wine.find_by_id(params[:id])
-        
-        
         @wine_review = @review.wine_id
-
-        
-        
 
         if @review.update(review_params)
             redirect_to review_path(@review)
-            
         else
             render :edit
         end
@@ -79,7 +73,6 @@ class ReviewsController < ApplicationController
         @review = Review.find(params[:id])
         @review.destroy
         redirect_to user_path(@user)
-
     end
 
     private 
@@ -88,10 +81,5 @@ class ReviewsController < ApplicationController
         params.require(:review).permit([:title, :description, :user_id, :wine_id, :fruit, :tannin, :body, :balance, :acidity, :flavors, :other, :color, :taste, :balance_rating, :wine_experience, :someone_else_like, :total])
     end
 
-   
 
-
-
-
-
-end
+end 
