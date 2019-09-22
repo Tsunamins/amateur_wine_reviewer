ef09be280f18db467ce46db8a60d3c6541a982b0
Tsunamins <reillyamr@gmail.com>
2019-09-22 11:14:33 -0400

    add custom valid. for same wine name different vintage

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -6,14 +6,22 @@ class Wine < ActiveRecord::Base
     accepts_nested_attributes_for :reviews
 
     validates :wine_name, presence: true
-    validates :wine_name, uniqueness: true
+    
     validates :vintage, length: { is: 4 }
     validates :wine_name, length: { maximum: 100 }
     validates :winemaker, length: { maximum: 100 }
     validates :vineyard, length: { maximum: 100 }
     validates :region, length: { maximum: 100 }
     validates :alcohol, length: { maximum: 5 } 
-    validates :story, length: { maximum: 1000 } 
+    validates :story, length: { maximum: 1000 }
+    validate :wine_and_vintage 
 
     scope :rose, -> { where(style: 'rose') }
+
+    def wine_and_vintage 
+        wine = Wine.find_by(wine_name: wine_name, vintage: vintage)
+        if !!wine && wine != self 
+            errors.add(:vintage, 'has been added, choose a different wine or different vintage')
+        end 
+    end 
 end 
